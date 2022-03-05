1980f4eab2a54bf43336a24e4b1b944645021282
Karthik <jeefkarthik036@gmail.com>
2022-03-05 20:28:17 +0530

    UC2-ArthematicOp

--- Arthmatic_computation.sh
+++ Arthmatic_computation.sh
@@ -1,5 +1,10 @@
 #!bin/bash -x
 
+#TAKING INPUT FROM USER
 read -p "Enter first input: " a;
 read -p "Enter second input: " b;
 read -p "Enter third input: " c;
+
+#COMPUTE ARITHMETIC OPERATION
+z=$(( $a + $(($b * $c))))
+echo "Result:" $z
