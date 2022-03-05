50540b423c0f3295cb1c2a5f2e1f97d13e15abb1
Karthik <jeefkarthik036@gmail.com>
2022-03-05 22:31:50 +0530

    UC7-dict_to_arr

--- Arthmatic_computation.sh
+++ Arthmatic_computation.sh
@@ -7,22 +7,35 @@ read -p "Enter third input: " c;
 
 #COMPUTE ARITHMETIC OPERATION
 z=$(( $a + $(($b * $c))))
-echo "Result:" $z
+echo "Result1:" $z
 
 z1=$(( $(($a * $b)) + $c ))
-echo "Result:" $z1
+echo "Result2:" $z1
 
 z2=$(( $c + $(($a / $b))))
-echo "Result:" $z2
+echo "Result3:" $z2
 
 z3=$(( $($a % $b) + $c ))
-echo "Result:" $z3
+echo "Result4:" $z3
 
 #STORING IN DICTIONARY
 
 declare -A arthmaticOperation
 
-arthmaticOperation[0]=$z
-arthmaticOperation[1]=$z1
-arthmaticOperation[2]=$z2
-arthmaticOperation[3]=$z3
+arthmaticOperation[z]=$z
+arthmaticOperation[z1]=$z1
+arthmaticOperation[z2]=$z2
+arthmaticOperation[z3]=$z3
+
+echo ${arthmaticOperation[@]}
+echo ${!arthmaticOperation[@]}
+echo ${#arthmaticOperation[@]}
+
+#Storing dict values into array
+for((i=0; i<=${#arthmaticOperation[@]}; i++))
+do
+  array[i]=${arthmaticOperation[z$((i+1))]}
+done
+#TO PRINT
+echo "${array[@]}"
+
